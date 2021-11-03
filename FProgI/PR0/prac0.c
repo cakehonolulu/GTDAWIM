591d9ab1686753f6ede71d9c4cc678f2bad3ac12
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2021-11-03 17:38:00 +0100

    FProgI/PR0: Remove all Goodies and leave the practise as vanilla as possible

---
+++ FProgI/PR0/prac0.c
@@ -0,0 +1,138 @@
+/*
+	Compile using: gcc prac0.c -Wall -Wextra (Use -DDEBUG switch if you want to enable debugging if compiling w/o make,
+	else specify DEBUG=1 alongside make).
+
+	GCC Version (LLVM's Clang untested):
+	
+	$ gcc --version
+	gcc (Ubuntu 11.2.0-7ubuntu2) 11.2.0
+
+	$ lsb_release -a
+	No LSB modules are available.
+	Distributor ID:	Ubuntu
+	Description:	Ubuntu Jammy Jellyfish (development branch)
+	Release:		22.04
+	Codename:		jammy
+	
+	$ uname -a
+	Linux CS3003NS 5.14.15-051415-generic #202110270548 SMP Wed Oct 27 11:48:56 UTC 2021 x86_64 x86_64 x86_64 GNU/Linux
+*/
+
+#include <stdio.h>
+
+/*
+	main() function
+*/
+int main()
+{
+	// Using unsigned char to ensure less memory footprint
+	unsigned char months;
+
+	// Using unsigned short to ensure less memory footprint
+	unsigned short nbworkers;
+
+	// Using unsigned short to ensure less memory footprint
+	unsigned short bworkers;
+
+	// Using unsigned int to ensure less memory footprint
+	unsigned int materials;
+
+	// Using unsigned int to ensure less memory footprint
+	unsigned int travels;
+
+	// Using unsigned int to ensure less memory footprint
+	unsigned int rentalandservices;
+
+	// Using unsigned int to ensure less memory footprint
+	unsigned int fungiblematerials;
+
+	printf("Calculador de Pressupost\n");
+
+	printf("Per començar, introdueixi la duració total del projecte (en mesos): ");
+
+	/*
+		I specify scanf the %hhu modifier
+
+		%hhu ensures that scanf reads the integer the user inputs as an unsigned char 
+	*/
+	scanf("%hhu", &months);
+
+	// Compare wether months is larger than 48 or smaller than 6, in that case, exit the program
+	if (months < 6 || months > 48)
+	{
+		printf("Durada del projecte invàlida, torni a executar el programa!\n");
+		return 1;
+	}
+
+	printf("Ara introdueixi el nombre de persones no becades (2500€ al mes) contractades: ");
+
+	/*
+		I specify scanf the %hu modifier
+
+		%hu ensures that scanf reads the integer the user inputs as an unsigned short 
+	*/
+	scanf("%hu", &nbworkers);
+
+	printf("Ara introdueixi el nombre de persones becades (1200€ al mes) contractades: ");
+
+	/*
+		I specify scanf the %hu modifier
+
+		%hu ensures that scanf reads the integer the user inputs as an unsigned short 
+	*/
+	scanf("%hu", &bworkers);
+
+	printf("Introdueixi la despesa total en material inventariable (€): ");
+
+	/*
+		I specify scanf the %u modifier
+
+		%u ensures that scanf reads the integer the user inputs as an unsigned int 
+	*/
+	scanf("%u", &materials);
+
+	printf("Introdueixi la despesa total en viatges (€): ");
+
+	/*
+		I specify scanf the %u modifier
+
+		%u ensures that scanf reads the integer the user inputs as an unsigned int 
+	*/
+	scanf("%u", &travels);
+
+	printf("Introdueixi la despesa total en alquilers i serveis (€): ");
+
+	/*
+		I specify scanf the %u modifier
+
+		%u ensures that scanf reads the integer the user inputs as an unsigned int 
+	*/
+	scanf("%u", &rentalandservices);
+
+	printf("Introdueixi la despesa total en materials fungibles (€): ");
+
+	/*
+		I specify scanf the %u modifier
+
+		%u ensures that scanf reads the integer the user inputs as an unsigned int 
+	*/
+	scanf("%u", &fungiblematerials);
+
+	unsigned int total = (((nbworkers) * 2500) + ((bworkers) * 1200) + (materials)
+		+ (travels) + (rentalandservices) + (fungiblematerials));
+
+	if (months >= 6 && months <= 12)
+	{
+		printf("S'aplica un overhead del 10%% al pressupost!\n");
+		float pct10 = (float) total * 0.1;
+		total = total + pct10;
+	} else {
+		printf("S'aplica un overhead del 20%% al pressupost!\n");
+		float pct20 = (float) total * 0.2;
+		total = total + pct20;
+	}
+
+	printf("El pressupost total per al projecte (Amb una durada de %u mesos) es: %0.2f (€)\n", (unsigned int) months, (float) total);
+
+	return 0;
+}
