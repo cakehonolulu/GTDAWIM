7fa90a9a2d929039f6b5bc03c829627e49520413
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-01-11 12:34:05 +0100

    FProgI/PR1/ Read the first sentence!

--- FProgI/PR1/prac1.c
+++ FProgI/PR1/prac1.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "prac1.h"
 
 int main()
 {
 	char m_filename[20] = {0};
+	int m_speed;
+	int m_yodification;
 	FILE *m_file = NULL;
 
 	printf("Yodizer - Del català al dialecte yoda!\n");
@@ -23,6 +24,40 @@ int main()
 		exit(EXIT_FAILURE);
 	}
 
+	printf("A continuació, introdueixi la velocitat del mestre Yoda: ");
+
+	scanf("%1d", &m_speed);
+
+	if (m_speed != 0 && m_speed != 1 && m_speed != 2)
+	{
+		printf("Paràmetre de velocitat no acceptat (0-3)!\nSortint...\n");
+		return 1;
+	}
+
+	printf("Ara, el coficient de yodificació: ");
+
+	scanf("%1d", &m_yodification);
+
+	if (m_yodification != 0 && m_yodification != 1 && m_yodification != 2)
+	{
+		printf("Paràmetre de velocitat no acceptat (0-3)!\nSortint...\n");
+		return 1;
+	}
+
+	yodify(m_file, m_speed, m_yodification);
+
 	fclose(m_file);
 
+	return 0;
+}
+
+int yodify(FILE *m_file, char m_speed, char m_yodification)
+{
+	char m_sentence[(15 * 15) + 1];
+	char m_lines;
+
+	fscanf(m_file, "%[^\n]", m_sentence);
+    printf("Primera frase:\n%s\n", m_sentence);
+
+	return 0;
 }
