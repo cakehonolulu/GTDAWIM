408588a8fe3bd66e99877bf8c07f2108efc38ef8
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-03-09 17:23:54 +0100

    FProgII/L2 -> dades.h: Fix function declaration bug in obtenir_dades(), min and max were swapped

--- FProgII/L2/dades.h
+++ FProgII/L2/dades.h
@@ -2,11 +2,11 @@
  * Funcio que emplena la taula amb 'nv' enters aleatoris
  * Els valors estan entre max i min ambdos inclosos
  * @param taula (Ref. (S): taula d'enters). La taula a emplenar
- * @param nv (Valor (E): enter). El nombre d'elements efectius 
- * @param max (Valor (E): enter). El valor maxim que prenen els valors
+ * @param nv (Valor (E): enter). El nombre d'elements efectius
  * @param min (Valor (E): enter). El valor minim que prenen els valors
+ * @param max (Valor (E): enter). El valor maxim que prenen els valors
  */ 
-void obtenir_dades(int taula[], int nv, int max, int min);    
+void obtenir_dades(int taula[], int nv, int min, int max);    
 
 /** 
  * Funcio que mostra per pantalla les dades
