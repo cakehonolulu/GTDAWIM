07e3997ea9499d7610397d55f3d1ea2b7c529def
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-03-09 16:00:05 +0100

    FProgII/L2: Start building the program

--- FProgII/L2/calculs.c
+++ FProgII/L2/calculs.c
@@ -12,3 +12,26 @@ float mitjana(int taula[], int nv)
 
 	return (m_result / nv); 
 }
+
+void maxmin(int taula[], int nv, int *max, int *min)
+{
+	int i = 0;
+
+	*min = taula[i];
+	*max = taula[i];
+
+	while (i < nv)
+	{
+		if (taula[i] > *min && taula[i] > *max)
+		{
+			*max = taula[i];
+		}
+		else
+		if (taula[i] < *min)
+		{
+			*min = taula[i];
+		}
+		
+		i++;
+	}
+}
--- FProgII/L2/lab2fp2.c
+++ FProgII/L2/lab2fp2.c
@@ -1,6 +1,33 @@
 #include <calculs.h>
+#include <stdio.h>
+#include <stdbool.h>
 
 int main()
 {
+	int m_vec[50] = {0};
+	int m_num = 0;
+	bool m_cond = true;
+
+	printf("Quants enters vol introduir (Mínim 1, Máxim 50)? ");
+
+	while (m_cond)
+	{
+		scanf("%d", &m_num);
+
+		if (m_num < 1)
+		{
+			printf("El nombre %d es massa petit!\n", m_num);
+		}
+		else
+		if (m_num > 50)
+		{
+			printf("El nombre %d es massa gran!\n", m_num);
+		}
+		else
+		{
+			m_cond = false;
+		}
+	}
+
 	return 0;
 }
