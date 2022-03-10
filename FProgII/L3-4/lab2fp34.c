32ec7b95877e8473b5d62d4ec83ab901cdd4edab
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-03-10 15:22:48 +0100

    FProgII/L3-4: Added NOTES and benchmark

--- FProgII/L3-4/lab2fp34.c
+++ FProgII/L3-4/lab2fp34.c
@@ -12,6 +12,8 @@
 
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main()
 {
@@ -22,33 +24,45 @@ int main()
 	unsigned int m_table[P] = {0};
 #endif
 
-	printf("Introdueixi un nombre: ");
-	scanf("%d", &m_number);
+	srand(time(NULL));
+	m_number = rand()%((5000+1)-1) + 1;
 
 	if (m_number != 0)
 	{
 #ifdef BRUTEFORCE
-		m_triangular = es_triangular_fb(m_number);
+		for (int i = 0; i < 10000; i++)
+		{
+			m_number = rand()%((5000+1)-1) + 1;
+			m_triangular = es_triangular_fb(m_number);
+		}
 #endif
 
 #ifdef OPTIMAL
-		m_triangular = es_triangular_op(m_number);
+		for (int i = 0; i < 10000; i++)
+		{
+			m_number = rand()%((5000+1)-1) + 1;
+			m_triangular = es_triangular_op(m_number);	
+		}
 #endif
 
 #ifdef TABLE
-		calcula_triangulars(m_table);
-		m_triangular = es_triangular_tau(m_table, m_number);
+		for (int i = 0; i < 10000; i++)
+		{
+			m_number = rand()%((5000+1)-1) + 1;
+			calcula_triangulars(m_table);
+			m_triangular = es_triangular_tau(m_table, m_number);		
+		}
 #endif
 	}
 
-	if (m_triangular)
+	/*if (m_triangular)
 	{
 		printf("El nombre %d es triangular!\n", m_number);
 	}
 	else
 	{
 		printf("El nombre %d no es triangular!\n", m_number);
-	}
+	}*/
 
 	return 0;
 }
