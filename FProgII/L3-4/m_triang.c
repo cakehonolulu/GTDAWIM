640be110f4d1748745937be3664164088a85ee1a
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-03-14 14:49:30 +0100

    FProgII/L3-4 -> m_triang: Simple utility that returns the triangular number at m_num position, also checks if < UINT32_MAX and returns it's value as hex to use in the main project

---
+++ FProgII/L3-4/m_triang.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdint.h>
+
+int main()
+{
+	unsigned int m_num = 0, m_triang = 0, i;
+
+	printf("Escriu la posició del triangular a obtenir: ");
+	scanf("%d", &m_num);
+
+	for (i = 0; i <= m_num; i++)
+	{
+		m_triang = (m_triang + i);
+	}
+
+	printf("El triangular a la posició %d és: %d\n", m_num, m_triang);
+
+	if (m_triang <= UINT32_MAX)
+	{
+		printf("Vàlid, més petit que 0x%06X (UINT32_MAX)\nValor constant per al programa: 0x%06X\n", UINT32_MAX, m_triang);
+	}
+
+	return 0;
+}
