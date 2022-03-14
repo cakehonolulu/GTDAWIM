d9e5e8776bebcdbd5ea3452e8cfeeed30ded7a3e
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-03-14 15:17:14 +0100

    FProgII/L3-4 -> lab2fp34: Minor code path optimizations

--- FProgII/L3-4/lab2fp34.c
+++ FProgII/L3-4/lab2fp34.c
@@ -13,10 +13,10 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <time.h>
 
 #define N_VEGADES 10000000
-#define M_MAX_NUM 50000
 
 int main()
 {
@@ -31,23 +31,15 @@ int main()
 	unsigned int m_table[P] = {0};
 #endif
 
-#ifdef BENCHMARK
-	srand(time(0));
-	m_number = rand() % ((M_MAX_NUM + 1) - 1) + 1;
-#else
+#ifndef BENCHMARK
 	while (m_cond)
 	{
 		printf("Introdueixi un nombre [0, 50000]: ");
 		scanf("%d", &m_number);
 	
-		if (m_number < 0)
+		if (m_number <= 0)
 		{
-			printf("El nombre ha de ser més gran que 0\n");
-		}
-		else
-		if (m_number > 50000)
-		{
-			printf("El nombre ha de ser més petit que 50000 (Inclós)!\n");
+			printf("El nombre ha de ser més gran o igual que 0\n");
 		}
 		else
 		{
@@ -56,59 +48,52 @@ int main()
 	}
 #endif
 
-#ifdef BENCHMARK
-	if (m_number > 0 && m_number < 50000)
-	{
-#endif
 #ifdef BRUTEFORCE
 #ifndef BENCHMARK
-		m_triangular = es_triangular_fb(m_number);
+	m_triangular = es_triangular_fb(m_number);
 #else
-		for (int i = 0; i < N_VEGADES; i++)
-		{
-			srand(time(0));
-			m_number = rand() % ((M_MAX_NUM + 1) - 1) + 1;
-			es_triangular_fb(m_number);
-		}
+	for (int i = 0; i < N_VEGADES; i++)
+	{
+		srand(time(0));
+		m_number = rand() % ((UINT32_MAX + 1) - 1) + 1;
+		es_triangular_fb(m_number);
+	}
 #endif
 #endif
 
 #ifdef OPTIMAL
 #ifndef BENCHMARK
-		m_triangular = es_triangular_op(m_number);
+	m_triangular = es_triangular_op(m_number);
 #else
-		for (int i = 0; i < N_VEGADES; i++)
-		{
-			srand(time(0));
-			m_number = rand() % ((M_MAX_NUM + 1) - 1) + 1;
-			es_triangular_op(m_number);	
-		}
+	for (int i = 0; i < N_VEGADES; i++)
+	{
+		srand(time(0));
+		m_number = rand() % ((UINT32_MAX + 1) - 1) + 1;
+		es_triangular_op(m_number);	
+	}
 #endif
 #endif
 
 #ifdef TABLE
 #ifndef BENCHMARK
-		calcula_triangulars(m_table);
-		m_triangular = es_triangular_tau(m_table, m_number);
+	calcula_triangulars(m_table);
+	m_triangular = es_triangular_tau(m_table, m_number);
 #else
-		calcula_triangulars(m_table);
+	calcula_triangulars(m_table);
 
-		for (int i = 0; i < N_VEGADES; i++)
-		{
-			srand(time(0));
-			m_number = rand() % ((M_MAX_NUM + 1) - 1) + 1;
+	for (int i = 0; i < N_VEGADES; i++)
+	{
+		srand(time(0));
+		m_number = rand() % ((UINT32_MAX + 1) - 1) + 1;
 			
-			if (m_number <= 0x4A81DE28)
-			{
-				es_triangular_tau(m_table, m_number);	
-			}
+		if (m_number <= 0x4A81DE28)
+		{
+			es_triangular_tau(m_table, m_number);	
 		}
-#endif
-#endif
-
-#ifdef BENCHMARK
 	}
 #endif
+#endif
+
 
 #ifndef BENCHMARK
 	if (m_triangular)
