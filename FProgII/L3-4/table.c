a99640fe6a246a7955ce4cadfef96e2ac3dff3b5
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-03-15 12:11:02 +0100

    FProgII/L3-4 -> table: Add code comments

--- FProgII/L3-4/table.c
+++ FProgII/L3-4/table.c
@@ -1,8 +1,11 @@
 #include <table.h>
-#include <stdio.h>
 
 void calcula_triangulars(unsigned int *m_table)
 {
+	/*
+		This function works the same as bruteforce, but whatever it computes
+		goes into an integer array[P]
+	*/
 	unsigned int m_summation = 0, i;
 
 	for (i = 0; i < P; i++)
@@ -26,11 +29,14 @@ bool es_triangular_tau(unsigned int *m_table, unsigned int m_number)
 	bool m_cond = true;
 	int i = 0;
 
+	// Do while condition lasts
 	while (m_cond)
 	{
+		// Compare each table's index value against the provide number
 		if (m_table[i] == m_number)
 		{
 #ifdef BENCHMARK
+			// Return the value
 			m_triangular = m_table[i];
 #else
 			m_triangular = true;
@@ -38,9 +44,11 @@ bool es_triangular_tau(unsigned int *m_table, unsigned int m_number)
 			m_cond = false;
 		}
 		else
+		// If bigger, stop
 		if (m_table[i] > m_number)
 		{
 #ifdef BENCHMARK
+			// But if on benchmark mode, assign the value (first_triang >= n)
 			m_triangular = m_table[i];
 #endif
 			m_cond = false;
@@ -48,6 +56,10 @@ bool es_triangular_tau(unsigned int *m_table, unsigned int m_number)
 
 		i++;
 	}
-
+	
+	/*
+		This only adds an additional instruction (Moves the result to %rax)
+		following x86_64 SysV ABI, at the end 1 instruction shouldn't be noticed at all
+	*/
 	return m_triangular;
 }
