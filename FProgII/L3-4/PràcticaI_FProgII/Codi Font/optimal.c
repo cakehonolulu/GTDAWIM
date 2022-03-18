9b0dcb6cd3deabe0d7596c904a0bc1fd168f80ee
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-03-18 09:22:31 +0100

    FProgII/L3-4 -> CodiFont: Add the optimized (Again) version

--- FProgII/L3-4/PràcticaI_FProgII/Codi Font/optimal.c
+++ FProgII/L3-4/PràcticaI_FProgII/Codi Font/optimal.c
@@ -2,8 +2,7 @@
 
 unsigned int es_triangular_op(unsigned int m_number)
 {
-	unsigned int m_triangular = 0;
-	unsigned int i;
+	unsigned int m_triangular = 0, i;
 
 	/*
 		Use a float, less memory footprint than using a double
@@ -13,7 +12,9 @@ unsigned int es_triangular_op(unsigned int m_number)
 		n = ----------------
 				   2
 	*/
-	float n = (((sqrtf((8 * m_number) + 1)) - 1) / 2);
+	float m_nearest, m_result;
+
+	m_result = (((sqrtf((8 * m_number) + 1)) - 1) / 2)
 
 	/*
 		This is an interesting trick:
@@ -21,26 +22,14 @@ unsigned int es_triangular_op(unsigned int m_number)
 		If the ceil'd version of n and the floor'd version of n are the same,
 		it's safe to assume n is a natural number without fractional part.
 	*/
-	if (ceil(n) == floor(n))
+	if (ceil(m_result) == floor(m_result))
 	{
-		m_triangular = (unsigned int) n;
+		m_triangular = (unsigned int) m_result;
 	}
 	else
 	{
-		/*
-			If it's not a triangular number, find the next one.
-			Reconstruct the original formula to get the original number.
-			Keep feeding values until the next closer triangular is found (Not the previous!)
-		*/
-		for (i = 0; n <= m_number; i++)
-		{
-			n = (((floor(sqrtf(2 * (m_number + i))))) * (((floor(sqrtf(2 * (m_number + i))))) + 1) / 2);
-
-			if (ceil(n) == floor(n) && (n >= m_number))
-			{
-				m_triangular = (unsigned int) n;
-			}
-		}
+		m_nearest = ceil(((sqrtf(1 + 8 * m_number)) - 1) / 2);
+    	m_nearest = ((m_nearest * (m_nearest + 1)) / 2);
 	}
 
 	/*
